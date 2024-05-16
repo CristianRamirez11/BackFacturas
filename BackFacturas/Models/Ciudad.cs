0bb6eef24791da6ee6a3d26062117daaeb0f5d67
Cristian Ramirez <cdrz01@gmail.com>
2024-05-16 01:50:13 -0500

    Update of the models of systems

--- BackFacturas/Models/Ciudad.cs
+++ BackFacturas/Models/Ciudad.cs
@@ -1,6 +1,13 @@
-﻿namespace BackFacturas.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace BackFacturas.Models
 {
     public class Ciudad
     {
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        public string Departamento { get; set; }
     }
 }
--- BackFacturas/Models/Factura.cs
+++ BackFacturas/Models/Factura.cs
@@ -1,11 +1,17 @@
-﻿namespace BackFacturas.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackFacturas.Models
 {
     public class Factura
     {
         public int Id { get; set; }
+        [Required]
         public int NumeroFactura { get; set; }
-        public int NombreArticulo { get; set; }
-        public int Ciudad { get; set; }
+        [Required]
+        public Articulo Articulo { get; set; }
+        [Required]
+        public Ciudad Ciudad { get; set; }
+        [Required]
         public double Valor { get; set; }
     }
 }
