a1d59fea9c4c16fb2f9ff9a64546666874843d29
Cristian Ramirez <cdrz01@gmail.com>
2024-05-16 01:54:42 -0500

    Update models Articulo

--- BackFacturas/Models/Articulo.cs
+++ BackFacturas/Models/Articulo.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.Identity.Client;
+using System.ComponentModel.DataAnnotations;
 
 namespace BackFacturas.Models
 {
     public class Articulo
     {
         public int Id { get; set; }
+        [Required]
         public string Nombre { get; set; }
+        [Required]
         public double Precio { get; set; }
         public int Cantidad { get; set; }
         
