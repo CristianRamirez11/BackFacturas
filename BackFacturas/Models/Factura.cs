f2f3e34a6b8b7d8dac09c798eeca89792f9b8579
Cristian Ramirez <cdrz01@gmail.com>
2024-05-16 01:31:50 -0500

    Se crear la clase Factura, la cual me describe una factura de venta dentro del sistema

---
+++ BackFacturas/Models/Factura.cs
@@ -0,0 +1,11 @@
+﻿namespace BackFacturas.Models
+{
+    public class Factura
+    {
+        public int Id { get; set; }
+        public int NumeroFactura { get; set; }
+        public int NombreArticulo { get; set; }
+        public int Ciudad { get; set; }
+        public double Valor { get; set; }
+    }
+}
