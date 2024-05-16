fc1de8e98715790d79e3748e06f4394a0f44730d
Cristian Ramirez <cdrz01@gmail.com>
2024-05-16 01:39:12 -0500

    Se crear la clase Articulo, representa los productos dentro del sistema

---
+++ BackFacturas/Models/Articulo.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Identity.Client;
+
+namespace BackFacturas.Models
+{
+    public class Articulo
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+        public int Cantidad { get; set; }
+        
+    }
+}
