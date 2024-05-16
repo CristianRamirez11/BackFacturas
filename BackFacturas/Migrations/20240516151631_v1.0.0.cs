e406aa41baa47a2aad87a5f74218ffa2b041954e
Cristian Ramirez <cdrz01@gmail.com>
2024-05-16 10:26:48 -0500

    se crea la migracion de la base de datos en la version 1.0.0

---
+++ BackFacturas/Migrations/20240516151631_v1.0.0.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackFacturas.Migrations
+{
+    /// <inheritdoc />
+    public partial class v100 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Articulo",
+                columns: table => new
+                {
+                    ArticuloId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Precio = table.Column<double>(type: "float", nullable: false),
+                    Cantidad = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articulo", x => x.ArticuloId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ciudade",
+                columns: table => new
+                {
+                    CiudadId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Departamento = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ciudade", x => x.CiudadId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Factura",
+                columns: table => new
+                {
+                    FacturaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NumeroFactura = table.Column<int>(type: "int", nullable: false),
+                    ValorTotal = table.Column<double>(type: "float", nullable: false),
+                    ArticuloId = table.Column<int>(type: "int", nullable: false),
+                    CiudadId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Factura", x => x.FacturaId);
+                    table.ForeignKey(
+                        name: "FK_Factura_Articulo_ArticuloId",
+                        column: x => x.ArticuloId,
+                        principalTable: "Articulo",
+                        principalColumn: "ArticuloId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Factura_Ciudade_CiudadId",
+                        column: x => x.CiudadId,
+                        principalTable: "Ciudade",
+                        principalColumn: "CiudadId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Factura_ArticuloId",
+                table: "Factura",
+                column: "ArticuloId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Factura_CiudadId",
+                table: "Factura",
+                column: "CiudadId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Factura");
+
+            migrationBuilder.DropTable(
+                name: "Articulo");
+
+            migrationBuilder.DropTable(
+                name: "Ciudade");
+        }
+    }
+}
