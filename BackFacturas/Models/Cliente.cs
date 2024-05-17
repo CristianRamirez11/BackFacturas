3b09df5e0d989e566180159234c5eb2b827d8902
Cristian Ramirez <cdrz01@gmail.com>
2024-05-17 12:53:45 -0500

    Se agregan la clase Cliente y DetalleFactura al modelo de dominio

---
+++ BackFacturas/Models/Cliente.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackFacturas.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int ClienteId { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string Apellido { get; set; }
+        [Required]
+        public string Celular { get; set; }
+        public string Email { get; set; }
+        [Required]
+        public int CiudadId { get; set; }
+        [Required]
+        public Ciudad Ciudad { get; set; }
+
+        public IList<Factura> Facturas { get; set; }
+
+        public Cliente()
+        {
+            Facturas = new List<Factura>();
+        }
+    }
+}
---
+++ BackFacturas/Models/DetalleFactura.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackFacturas.Models
+{
+    public class DetalleFactura
+    {
+        [Key]
+        public int NumeroDetalle { get; set; }
+        [Required]
+        public int Cantidad { get; set; }
+        [Required]
+        public double ValorTotal { get; set; }
+        [Required]
+        public int NumeroFactura { get; set; }
+        [Required]
+        public Factura Factura { get; set; }
+        [Required]
+        public int ArticuloId { get; set; }
+        [Required]
+        public Articulo Articulo { get; set; }
+
+    }
+}
