81c22c584142c66777288144a7fc7098c4ed9be6
Cristian Ramirez <cdrz01@gmail.com>
2024-05-17 19:14:28 -0500

    Se agrega nuevo DTO para el manejo de actualizacion de facturas

---
+++ BackFacturas/DTOs/ActualizarDetalleFacturaDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace BackFacturas.DTOs
+{
+    public class ActualizarDetalleFacturaDTO
+    {
+        public int NumeroFactura { get; set; }
+        public int NumeroDetalle { get; set; }
+        public int Cantidad { get; set; }
+        public double ValorTotal { get; set; }
+        public string NombreCiudad { get; set; }
+        public string NombreArticulo { get; set; }
+    }   
+}
